fc5a2fd1345f721f9188af88be3a998347648266
Kira9112 <kira91_911@mail.ru>
2021-09-24 14:21:17 +0300

    Added Tests

---
+++ src/test/java/ru/netology/Tests.java
@@ -0,0 +1,58 @@
+package ru.netology;
+
+
+import lombok.val;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class Tests {
+
+    @BeforeEach
+    void before() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    void inputValidLogPasStatActive() {
+        val user = DataGenerator.dataName("active");
+        DataGenerator.setUpAll(user);
+        $("span[data-test-id='login'] input").setValue(user.getLogin());
+        $("span[data-test-id='password'] input").setValue(user.getPassword());
+        $("button[data-test-id='action-login']").click();
+        $("h2.heading.heading_size_l.heading_theme_alfa-on-white").shouldHave(text("Личный кабинет"));
+    }
+
+    @Test
+    void inputValidLogPasStatBlocked() {
+        val user = DataGenerator.dataName("blocked");
+        DataGenerator.setUpAll(user);
+        $("span[data-test-id='login'] input").setValue(user.getLogin());
+        $("span[data-test-id='password'] input").setValue(user.getPassword());
+        $("button[data-test-id='action-login']").click();
+        $("[data-test-id=error-notification]").shouldHave(text("Пользователь заблокирован"));
+    }
+
+    @Test
+    void inputNo_Valid_LogValidPasStatActive() {
+        val user = DataGenerator.dataName("active");
+        DataGenerator.setUpAll(user);
+        $("span[data-test-id='login'] input").setValue(DataGenerator.noValidLog());
+        $("span[data-test-id='password'] input").setValue(user.getPassword());
+        $("button[data-test-id='action-login']").click();
+        $("[data-test-id=error-notification]").shouldHave(text("Неверно указан логин или пароль"));
+    }
+    @Test
+    void inputValidLogNo_Valid_PasStatActive() {
+        val user = DataGenerator.dataName("active");
+        DataGenerator.setUpAll(user);
+        $("span[data-test-id='login'] input").setValue(user.getLogin());
+        $("span[data-test-id='password'] input").setValue(DataGenerator.noValidPass());
+        $("button[data-test-id='action-login']").click();
+        $("[data-test-id=error-notification]").shouldHave(text("Неверно указан логин или пароль"));
+    }
+}
